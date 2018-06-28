fccebc62fb16234ca68b2b8ffd4eb452ecf7d67c
Matt <matthew.sommer@gmail.com>
2018-06-27 19:50:33 -0700

    started added conditional status icons

--- src/pages/reading.js
+++ src/pages/reading.js
@@ -5,6 +5,16 @@ function importAll(r) {
     return r.keys().map(r);
 }
 
+function StatusIcon(status) {
+    if (status == 'Open') {
+        return <img src={photography[0]} alt="icon" className="status" />;
+    } else if (status == 'Closed') {
+        return <img src={photography[1]} alt="icon" className="status" />;
+    } else if (status == 'In Progress') {
+        return <img src={photography[2]} alt="icon" className="status" />;
+    }
+}
+
 const photography = importAll(require.context('./', false, /\.(png|jpe?g|svg)$/));
 
 const ReadingPage = (props) => {
@@ -17,7 +27,7 @@ const ReadingPage = (props) => {
                 const taskNode = task.node;
                 return (
                     <div key={i}>
-                        {taskNode.jiraIssue.jiraFields.status.name == 'Open' ? <img src={photography[0]} alt="icon" className="status"/> : <img src={photography[1]} alt="icon" className="status"/>}
+                        {StatusIcon(taskNode.jiraIssue.jiraFields.status.name)}
                         <Link to={'/' + taskNode.slug}>{taskNode.jiraIssue.jiraFields.summary}</Link> by {taskNode.jiraIssue.jiraFields.customfield_10100}
                     </div>
                 )
